e4e67028c0073966e43b7b5eba33fd6092fec83d
timmyventura <petimov@github.com>
2017-07-10 16:32:15 +0300

    Update install bash script

--- bash/SimpleNetworkMonitor.sh
+++ bash/SimpleNetworkMonitor.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
+
+
 JAVA_ORACLE='Java(TM)'
 JAVA_OPENJDK='OpenJDK'
+DIRECTORY='.';
+PATHS=$0
 
 CHECK_JAVA_VERSION_ARCH=`java -version 2>&1 | awk '/Bit/{print $3}'`	
 CHECK_JAVA_VERSION=`java -version 2>&1 | grep 'version "1.8.*"' | wc -l`
 
+
+
+define_path(){
+
+if [ "${PATHS:0:1}" = '/' ]; then
+  DIRECTORY="${PATHS%/*}"
+else
+ TEMP="$PWD/${PATHS}"
+ DIRECTORY="${TEMP%/*}"
+fi
+
+}
+
+install_java(){
+
+    echo "You have not installed java version 8 instance"
+    echo "Download/Update java, please type sudo password and wait, while java version 8 installed/update"
+    sudo apt-get update
+    sudo apt-get -y install software-properties-common
+    sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
+    sudo apt-get update
+    sudo apt-get -y install oracle-java8-installer
+    sudo apt-get -y install oracle-java8-set-default
+
+}
+
+
+define_path
 
 if [ ${CHECK_JAVA_VERSION} != 0 ]; then
   
@@ -13,21 +45,19 @@ if [ ${CHECK_JAVA_VERSION} != 0 ]; then
 
  if [ `java -version 2>&1 | grep ${JAVA_ORACLE} | wc -l` != 0 ]; then
    
-  echo "Java version 8 developed by Oracle"
+ echo "Java version 8 developed by Oracle"
    
     if [ ${CHECK_JAVA_VERSION_ARCH} == '64-Bit' ]; then
 
       echo "Java version 8 64 bit architecture"
       echo "Run application..."
-
-      sudo java -Djava.library.path=./library/lin64 -jar SimpleNetworkMonitor.jar
+      sudo java -Djava.library.path=$DIRECTORY/library/lin64 -jar $DIRECTORY/SimpleNetworkMonitor.jar
 
     else
 
-       echo "Java version 8 32 bit architecture"
-       echo "Run application..."
-
-      sudo java -Djava.library.path=./library/lin32 -jar SimpleNetworkMonitor.jar
+      echo "Java version 8 32 bit architecture"
+      echo "Run application..."
+      sudo java -Djava.library.path=$DIRECTORY/library/lin32 -jar $DIRECTORY/SimpleNetworkMonitor.jar
 
     fi
 
@@ -40,28 +70,21 @@ if [ ${CHECK_JAVA_VERSION} != 0 ]; then
       echo "Java version 8 64 bit architecture"
       echo "Run application..."
 
-      sudo java -Djava.library.path=./library/lin64 -jar SimpleNetworkMonitor.jar
+      sudo java -Djava.library.path=$DIRECTORY/library/lin64 -jar $DIRECTORY/SimpleNetworkMonitor.jar
 
     else
 
       echo "Java version 8 32 bit architecture"
       echo "Run application..."
 
-      sudo java -Djava.library.path=./library/lin32 -jar SimpleNetworkMonitor.jar
+      sudo java -Djava.library.path=$DIRECTORY/library/lin32 -jar $DIRECTORY/SimpleNetworkMonitor.jar
 
     fi
 
   fi
 
 else
-    echo "You have not installed java version 8 instance"
-    echo "Download/Update java, please type sudo password and wait, while java version 8 installed/update"
-    sudo apt-get update
-    sudo apt-get -y install software-properties-common
-    sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
-    sudo apt-get update
-    sudo apt-get -y install oracle-java8-installer
-    sudo apt-get -y install oracle-java8-set-default
+    install_java
 fi
 
 
--- bash/install.sh
+++ bash/install.sh
@@ -5,6 +5,48 @@
 JAVA_ORACLE='Java(TM)'
 JAVA_OPENJDK='OpenJDK'
 
+DIRECTORY='.';
+PATHS=$0
+
+NAME='simple_network_monitor.desktop'
+NAME_SCRIPT='SimpleNetworkMonitor.sh'
+
+define_path(){
+
+echo "Define appliaction path"
+
+if [ "${PATHS:0:1}" = '/' ]; then
+  DIRECTORY="${PATHS%/*}"
+else
+ TEMP="$PWD/${PATHS}"
+ DIRECTORY="${TEMP%/*}"
+fi
+
+}
+ 
+create_icon(){
+ 
+echo "Create icon"
+
+echo '[Desktop Entry]' >> "$DIRECTORY/$NAME"
+echo 'Name=SimpleNetworkMonitor' >> "$DIRECTORY/$NAME"
+echo 'Comment=A simple network monitor' >> "$DIRECTORY/$NAME"
+echo 'GenericName=SNM' >> "$DIRECTORY/$NAME"
+echo 'Keywords=Simple;Monitor' >> "$DIRECTORY/$NAME"
+echo "Exec=gksu $DIRECTORY/SimpleNetworkMonitor.sh" >> "$DIRECTORY/$NAME"
+echo 'Terminal=false' >> "$DIRECTORY/$NAME"
+echo 'Icon=/usr/share/pixmaps/javaws.png' >> "$DIRECTORY/$NAME"
+echo 'Type=Application' >> "$DIRECTORY/$NAME"
+echo 'NoDisplay=false' >> "$DIRECTORY/$NAME"
+
+echo "Use runnable $NAME_SCRIPT script"
+sudo chmod +x "$DIRECTORY/$NAME_SCRIPT"
+
+echo "Copy icon to share directory"
+sudo cp "$DIRECTORY/$NAME" /usr/share/applications
+
+}
+ 
    sudo apt-get update
    echo "Install net-tools packet"
    sudo apt-get -y install net-tools
@@ -14,12 +56,15 @@ JAVA_OPENJDK='OpenJDK'
    sudo apt-get -y install software-properties-common
    sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
    sudo apt-get update
-   #echo "Install oracle java 8 instance"
+   echo "Install oracle java 8 instance"
    if [ `java -version 2>&1 | grep ${JAVA_ORACLE} | wc -l` != 0 ]; then
    echo "Install oracle java 8 instance"
    sudo apt-get -y install oracle-java8-installer
    elif [ `java -version 2>&1 | grep ${JAVA_OPENJDK} | wc -l` != 0 ]; then
    echo "Install openjdk java 8 instance"
-   sudo apt-get -y install openjdk-8-jre 
+   sudo apt-get -y install openjdk-8-jre
+  
+   define_path
+   create_icon
    fi
    echo "Success!"
