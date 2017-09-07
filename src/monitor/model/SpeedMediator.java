2587c844b66b99d08687ada72007f8d73981d232
Timofey Petrov <daimler3323@gmail.com>
2017-09-07 09:57:26 +0300

    Update SpeedMediator.java

--- src/monitor/model/SpeedMediator.java
+++ src/monitor/model/SpeedMediator.java
@@ -14,10 +14,8 @@ import monitor.view.View;
 public class SpeedMediator extends AbstractModel {
 
 	private List<View> views;
-	
 	private static final Timer timer = new Timer();
-	
-    private static final int TIMEOUT = 1000;
+        private static final int TIMEOUT = 1000;
 	private int IN_LEN;
 	private int OUT_LEN;
 	private int MAX_IN_LEN;
@@ -53,8 +51,8 @@ public class SpeedMediator extends AbstractModel {
     	
     }
 	
-	@Override
-	public void execute(PcapPacket packet) {
+    @Override
+    public void execute(PcapPacket packet) {
 	   	
 		int wirelen = packet.getCaptureHeader().wirelen();
 			
@@ -66,19 +64,19 @@ public class SpeedMediator extends AbstractModel {
 			IN_LEN+=wirelen;
 		}
 		
-	}
+    }
 	
-	private Ethernet getEtherHeader(PcapPacket packet) {
+    private Ethernet getEtherHeader(PcapPacket packet) {
 		
         return (Ethernet)packet.getHeader((Ethernet)ProtocolSet.ETHERNET.getInstance());
 
-	}
+    }
 	
-	private boolean compareSrcMacWithInterfaceMac(PcapPacket packet) {
+    private boolean compareSrcMacWithInterfaceMac(PcapPacket packet) {
 		
 		return Arrays.equals(getEtherHeader(packet).source(), dev_mac);
 		
-	}
+     }
 	
     private void sendToView() {
 		
@@ -112,36 +110,36 @@ public class SpeedMediator extends AbstractModel {
     	
     }
     
-    private double getMaxOutputLength() {
+     private double getMaxOutputLength() {
     	
     	if(MAX_OUT_LEN<=OUT_LEN) MAX_OUT_LEN=OUT_LEN;
     	
         return (double)MAX_OUT_LEN*8/1024;
     	
-    }
+     } 
 
-	public void clear() {
+      public void clear() {
 		
 		IN_LEN=0;
 		OUT_LEN=0;
 		
-	}
+      }
 
-	@Override
-	public void addView(View view) {
+      @Override
+      public void addView(View view) {
 		
 		views.add(view);
 		
-	}
+      }
 
-	@Override
-	public boolean removeView(View view) {
+      @Override
+      public boolean removeView(View view) {
 	
 		return views.remove(view);
-	}
+      }
 
-	@Override
-	public void run() {
+      @Override
+      public void run() {
 		
 		
 		
