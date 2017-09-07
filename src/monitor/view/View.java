78c7042373f440b83ee932dd6768eef16edb413c
Timofey Petrov <daimler3323@gmail.com>
2017-09-07 10:36:46 +0300

    Update View.java

--- src/monitor/view/View.java
+++ src/monitor/view/View.java
@@ -7,7 +7,7 @@ public interface View {
 
 	public void addObservation(Map<Object, Object> param);
 	public void addObservation(double input_length, 
-			                   double output_length, 
-			                   double max_input_length,
-			                   double max_output_length);
+			           double output_length, 
+			           double max_input_length,
+			           double max_output_length);
 }
